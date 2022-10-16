26c56691a10f20a69e52496f5299b217f0d97b30
levi85 <joshua.zhangzy@Gmail.com>
2022-10-16 13:21:01 +0800

    Refactor Role object into a value object, key actions: 1) Remove abstract from Role object to meet JPA requirement for persistency 2) shifted contact number to user account 3) shifted isDisabled to Role object to handle the scenario where a borrower is disabled while still requiring other role functions e.g. Administrator. Having to disable instead of deletion helps to maintain data consistency for transactional records.

--- cpa/src/main/java/com/is442project/cpa/admin/Administrator.java
+++ cpa/src/main/java/com/is442project/cpa/admin/Administrator.java
@@ -1,10 +1,20 @@
 package com.is442project.cpa.admin;
 
-import javax.persistence.Entity;
+import javax.persistence.Embeddable;
 
-@Entity
+@Embeddable
 public class Administrator extends Role{
     public Administrator() {
         super("Admin");
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
--- cpa/src/main/java/com/is442project/cpa/admin/Borrower.java
+++ cpa/src/main/java/com/is442project/cpa/admin/Borrower.java
@@ -1,36 +1,10 @@
 package com.is442project.cpa.admin;
 
-import javax.persistence.Entity;
+import javax.persistence.Embeddable;
 
-@Entity
+@Embeddable
 public class Borrower extends Role{
-
-    private String contactNumber;
-    private boolean isDisabled;
-
     public Borrower() {
-        this("", false);
-    }
-
-    public Borrower(String contactNumber, boolean isDisabled) {
         super("Borrower");
-        this.contactNumber = contactNumber;
-        this.isDisabled = isDisabled;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public boolean isDisabled() {
-        return isDisabled;
-    }
-
-    public void setDisabled(boolean disabled) {
-        isDisabled = disabled;
     }
 }
