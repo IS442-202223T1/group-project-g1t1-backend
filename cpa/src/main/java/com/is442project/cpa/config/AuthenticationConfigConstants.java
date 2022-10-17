9da2ff579f3068a045e4abe097c4d27f9ff0f281
jonathantan1425 <jonathantan1425@gmail.com>
2022-10-17 21:24:37 +0800

    Shift authentication to separate folder

--- cpa/src/main/java/com/is442project/cpa/config/AuthenticationConfigConstants.java
+++
@@ -1,9 +0,0 @@
-package com.is442project.cpa.config;
-
-public class AuthenticationConfigConstants {
-    public static final String SECRET = AuthenticationSecretGenerator.generateSecret();
-    public static final long EXPIRATION_TIME = 864000000; // 10 days
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/api/v1/admin/create";
-}
