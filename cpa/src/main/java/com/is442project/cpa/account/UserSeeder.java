2613fd635b391f66b0374f908c361f4ff6671667
theianchia <theianchia@users.noreply.github.com>
2022-10-20 09:26:06 +0800

    Updated user seed data to facilitate FE testing

--- cpa/src/main/java/com/is442project/cpa/account/UserSeeder.java
+++ cpa/src/main/java/com/is442project/cpa/account/UserSeeder.java
@@ -18,10 +18,10 @@ public class UserSeeder {
     }
 
     public void insertTestData() {
-        UserAccount admin = new UserAccount("testAdmin@gmail.com", "TestAdmin", "testadmin", null,Arrays.asList(new Administrator(), new Borrower()));
+        UserAccount admin = new UserAccount("testAdmin@gmail.com", "TestAdmin", "testadmin", null, Arrays.asList(new Administrator(), new Borrower()));
 
-        UserAccount user01 = new UserAccount("david@sportsschool.edu.sg", "David Tan");
-        UserAccount user02 = new UserAccount("mary@nysi.org.sg", "Mary Lim");
+        UserAccount user01 = new UserAccount("david@sportsschool.edu.sg", "David Tan", "davidtan", null, Arrays.asList(new Borrower()));
+        UserAccount user02 = new UserAccount("mary@nysi.org.sg", "Mary Lim", "marylim", null, Arrays.asList(new Borrower()));
 
         userAccountRepository.saveAllAndFlush(Arrays.asList(admin, user01, user02));
 
