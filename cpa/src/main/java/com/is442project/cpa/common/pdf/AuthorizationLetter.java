ccaaeef458be0cd97edb8ec59ee6df991d83c1a9
levi85 <joshua.zhangzy@Gmail.com>
2022-10-30 00:53:53 +0800

    Update sentHtmlEmailWithAttachments test to use pdfFactory to send attachment instead from filesystem.

---
+++ cpa/src/main/java/com/is442project/cpa/common/pdf/AuthorizationLetter.java
@@ -0,0 +1,41 @@
+package com.is442project.cpa.common.pdf;
+
+import com.is442project.cpa.common.template.TemplateEngine;
+import com.is442project.cpa.common.template.TemplateResources;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+import java.io.IOException;
+
+public class AuthorizationLetter implements PdfTemplate{
+
+    private final PDDocument document = new PDDocument();
+    private final TemplateResources templateResources;
+
+    public AuthorizationLetter(TemplateResources templateResources) {
+        this.templateResources = templateResources;
+    }
+
+    @Override
+    public PDDocument getPdfDocument() {
+
+        TemplateEngine templateEngine = new TemplateEngine(templateResources);
+
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+            contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
+            contentStream.beginText();
+            contentStream.showText(templateEngine.getContent().replaceAll("\n", "").replaceAll("\r",""));
+            contentStream.endText();
+            contentStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return document;
+    }
+}
