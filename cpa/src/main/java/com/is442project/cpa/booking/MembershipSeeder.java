94a4a3ae784898bb4569896a519f14c1cbc4c57f
levi85 <joshua.zhangzy@Gmail.com>
2022-11-10 14:40:10 +0800

    amend friendly keyword for Templates to {{}} insetad of <>

--- cpa/src/main/java/com/is442project/cpa/booking/MembershipSeeder.java
+++ cpa/src/main/java/com/is442project/cpa/booking/MembershipSeeder.java
@@ -42,36 +42,30 @@ public class MembershipSeeder {
     public String buildSamplePhysicalPassEmailTemplate() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("Dear <borrower_name>,");
+        sb.append("Dear {{borrower_name}},");
         sb.append("<br><br>");
-        sb.append(" We are pleased to inform that your booking to <attraction name> is confirmed as follows:");
+        sb.append(" We are pleased to inform that your booking to {{attraction name}} is confirmed as follows:");
         sb.append("<br><br>");
-        sb.append("Date of Visit : <visit_date> (1day only)");
+        sb.append("Date of Visit : {{visit_date}} (1day only)");
         sb.append("<br>");
-        sb.append("Membership ID : <corp_pass_number>");
+        sb.append("Membership ID : {{corp_pass_number}}");
         sb.append("<br><br>");
-        sb.append(
-                "For any change in visit date, you are required to cancel your booking (at least 1 day before) and to submit a new booking in the system.");
+        sb.append("For any change in visit date, you are required to cancel your booking (at least 1 day before) and to submit a new booking in the system.");
         sb.append("<br><br>");
-        sb.append(
-                "Attached is the authorisation letter to <attraction_name>. Please check that the details are accurate.");
+        sb.append("Attached is the authorisation letter to {{attraction_name}}. Please check that the details are accurate.");
         sb.append("<br><br>");
-        sb.append("Please take note of the following for your visit to <attraction_name>:");
+        sb.append("Please take note of the following for your visit to {{attraction_name}}:");
         sb.append("<br><br>");
         sb.append("<li>Present this email confirmation to the General Office to collect the membership card(s). </li>");
-        sb.append(
-                "<li>Collect the membership card(s) from the General Office one day before your visit date and return the membership card(s) by 9am the next working day after your visit.</li>");
-        sb.append(
-                "<li>Present the membership card(s), the authorisation letter and your staff pass at the entrance of <attraction_name>.</li>");
+        sb.append("<li>Collect the membership card(s) from the General Office one day before your visit date and return the membership card(s) by 9am the next working day after your visit.</li>");
+        sb.append("<li>Present the membership card(s), the authorisation letter and your staff pass at the entrance of {{attraction_name}}.</li>");
         sb.append("<li>Entry is strictly based on the membership card(s) and the authorisation letter.</li>");
-        sb.append(
-                "<li>Your presence is required on the day of visit. Entry will be denied without staff’s presence.</li>");
+        sb.append("<li>Your presence is required on the day of visit. Entry will be denied without staff’s presence.</li>");
         sb.append("<li>Your booking is non-transferable.</li>");
-        sb.append(
-                "<li>Visit date is strictly based on the date stated in this email and the authorisation letter.</li>");
+        sb.append("<li>Visit date is strictly based on the date stated in this email and the authorisation letter.</li>");
         sb.append("<li>Staff found abusing the membership(s) will be subject to disciplinary actions.</li>");
         sb.append("<br><br>");
-        sb.append("Enjoy your visit to <attraction_name>!");
+        sb.append("Enjoy your visit to {{attraction_name}}!");
         sb.append("<br><br><br>");
         sb.append("Warm regards");
         sb.append("<br>");
@@ -83,36 +77,30 @@ public class MembershipSeeder {
     public String buildSampleElectronicPassEmailTemplate() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("Dear <borrower_name>,");
+        sb.append("Dear {{borrower_name}},");
         sb.append("<br><br>");
-        sb.append(" We are pleased to inform that your booking to <attraction name> is confirmed as follows:");
+        sb.append(" We are pleased to inform that your booking to {{attraction_name}} is confirmed as follows:");
         sb.append("<br><br>");
-        sb.append("Date of Visit : <visit_date> (1day only)");
+        sb.append("Date of Visit : {{visit_date}} (1day only)");
         sb.append("<br>");
-        sb.append("Membership ID : <corp_pass_number>");
+        sb.append("Membership ID : {{corp_pass_number}}");
         sb.append("<br><br>");
-        sb.append(
-                "For any change in visit date, you are required to cancel your booking (at least 1 day before) and to submit a new booking in the system.");
+        sb.append("For any change in visit date, you are required to cancel your booking (at least 1 day before) and to submit a new booking in the system.");
         sb.append("<br><br>");
-        sb.append(
-                "Attached is the authorisation letter to <attraction_name>. Please check that the details are accurate.");
+        sb.append("Attached is the authorisation letter to {{attraction_name}}. Please check that the details are accurate.");
         sb.append("<br><br>");
-        sb.append("Please take note of the following for your visit to <attraction_name>:");
+        sb.append("Please take note of the following for your visit to {{attraction_name}}:");
         sb.append("<br><br>");
         sb.append("<li>Present this email confirmation to the General Office to collect the membership card(s). </li>");
-        sb.append(
-                "<li>Collect the membership card(s) from the General Office one day before your visit date and return the membership card(s) by 9am the next working day after your visit.</li>");
-        sb.append(
-                "<li>Present the membership card(s), the authorisation letter and your staff pass at the entrance of <attraction_name>.</li>");
+        sb.append("<li>Collect the membership card(s) from the General Office one day before your visit date and return the membership card(s) by 9am the next working day after your visit.</li>");
+        sb.append("<li>Present the membership card(s), the authorisation letter and your staff pass at the entrance of {{attraction_name}}.</li>");
         sb.append("<li>Entry is strictly based on the membership card(s) and the authorisation letter.</li>");
-        sb.append(
-                "<li>Your presence is required on the day of visit. Entry will be denied without staff’s presence.</li>");
+        sb.append("<li>Your presence is required on the day of visit. Entry will be denied without staff’s presence.</li>");
         sb.append("<li>Your booking is non-transferable.</li>");
-        sb.append(
-                "<li>Visit date is strictly based on the date stated in this email and the authorisation letter.</li>");
+        sb.append("<li>Visit date is strictly based on the date stated in this email and the authorisation letter.</li>");
         sb.append("<li>Staff found abusing the membership(s) will be subject to disciplinary actions.</li>");
         sb.append("<br><br>");
-        sb.append("Enjoy your visit to <attraction_name>!");
+        sb.append("Enjoy your visit to {{attraction_name}}!");
         sb.append("<br><br><br>");
         sb.append("Warm regards");
         sb.append("<br>");
