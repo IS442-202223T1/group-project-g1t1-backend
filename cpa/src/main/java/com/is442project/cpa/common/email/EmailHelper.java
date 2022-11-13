db6f226d87f0d12489f2e920298e49b105159a27
levi85 <joshua.zhangzy@Gmail.com>
2022-11-13 19:30:53 +0800

    fix a small bug and seeded some data to test

--- cpa/src/main/java/com/is442project/cpa/common/email/EmailHelper.java
+++ cpa/src/main/java/com/is442project/cpa/common/email/EmailHelper.java
@@ -14,7 +14,7 @@ public class EmailHelper {
         sb.append("Dear " + booking.getBorrower().getName() + ",");
         sb.append("<br><br>");
         sb.append(String.format("Your Corporate Pass booking to %s for %s is cancelled because a borrower has lost the card.",
-                booking.getCorporatePass().getMembership().getMembershipName()));
+                booking.getCorporatePass().getMembership().getMembershipName(), booking.getBorrowDate().toString()));
 
         sb.append("<br><br><br>");
         sb.append("Warm regards");
