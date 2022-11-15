c07c730aff629d694b90c4db7fe3c2055a927a32
levi85 <joshua.zhangzy@Gmail.com>
2022-11-15 21:43:56 +0800

    Updates REST API endpoint should use PUT

--- cpa/src/main/java/com/is442project/cpa/config/GlobalConfigController.java
+++ cpa/src/main/java/com/is442project/cpa/config/GlobalConfigController.java
@@ -4,12 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/global-config")
@@ -27,7 +22,7 @@ public class GlobalConfigController {
       return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @PostMapping("")
+    @PutMapping("")
     @ResponseStatus(code = HttpStatus.OK)
     public ResponseEntity UpdateConfig(@RequestBody ConfigDTO ConfigDTO) {
         GlobalConfig result = globalConfigOps.updateConfig(ConfigDTO);
