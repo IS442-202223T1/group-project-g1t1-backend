3ae3a614018b486c9f329e2bb9dbe267adaaf127
levi85 <joshua.zhangzy@Gmail.com>
2022-11-17 09:31:31 +0800

    seeded test data for demo

--- cpa/src/main/java/com/is442project/cpa/account/seeder/UserSeeder.java
+++ cpa/src/main/java/com/is442project/cpa/account/seeder/UserSeeder.java
@@ -23,14 +23,24 @@ public class UserSeeder {
     }
 
     public void insertTestData() {
-        UserAccount admin = new UserAccount("testAdmin@gmail.com", "TestAdmin", "testadmin", null, Arrays.asList(new Administrator(), new Borrower(), new GeneralOfficePersonnel()));
+        //ADMIN
+        UserAccount admin = new UserAccount("ezra.chooi@gmail.com", "Ezra", "ezra", "81525893", Arrays.asList(new Administrator(), new Borrower(), new GeneralOfficePersonnel()));
 
-        UserAccount user01 = new UserAccount("david@sportsschool.edu.sg", "David Tan", "davidtan", null, Arrays.asList(new Borrower()));
-        UserAccount user02 = new UserAccount("mary@nysi.org.sg", "Mary Lim", "marylim", null, Arrays.asList(new Borrower()));
-        UserAccount user03 = new UserAccount("joshua.zhangzy@gmail.com", "Joshua", "Joshua", null, Arrays.asList(new Borrower()));
-        UserAccount user04 = new UserAccount("brotherbear@gmail.com", "Brother Bear", "brotherbear", "98988787", Arrays.asList(new Borrower()));
+        //GOP
+        UserAccount user01 = new UserAccount("david@sportsschool.edu.sg", "David Tan", "davidtan", "87451258", Arrays.asList(new GeneralOfficePersonnel()));
 
-        userAccountRepository.saveAllAndFlush(Arrays.asList(admin, user01, user02, user03, user04));
+        //Existing Borrower
+        UserAccount user02 = new UserAccount("joshua.zhangzy@gmail.com", "Joshua", "Joshua", "95268745", Arrays.asList(new Borrower()));
+        UserAccount user03 = new UserAccount("mary@nysi.org.sg", "Mary Tan", "Mary", "94568217", Arrays.asList(new Borrower()));
+        UserAccount user04 = new UserAccount("Carol@nysi.org.sg", "Carol Lee", "Carol", "94353255", Arrays.asList(new Borrower()));
+        UserAccount user05 = new UserAccount("Hugo@nysi.org.sg", "Hugo Ooi", "Hugo", "96528216", Arrays.asList(new Borrower()));
+        UserAccount user06 = new UserAccount("Kelvin@sportsschool.edu.sg", "Kelvin Lim", "Kelvin", "94598512", Arrays.asList(new Borrower()));
+        UserAccount user07 = new UserAccount("Kennedy@sportsschool.edu.sg", "Kennedy Teo", "Kennedy", "98546317", Arrays.asList(new Borrower()));
+        UserAccount user08 = new UserAccount("Emmy@sportsschool.edu.sg", "Emmy Goh", "Emmy", "94514552", Arrays.asList(new Borrower()));
+        UserAccount user09 = new UserAccount("Elliot@sportsschool.edu.sg", "Elliot Tan", "Elliot", "91269362", Arrays.asList(new Borrower()));
+        UserAccount user10 = new UserAccount("Willie@sportsschool.edu.sg", "Willie Sim", "Willie", "84565537", Arrays.asList(new Borrower()));
+
+        userAccountRepository.saveAllAndFlush(Arrays.asList(admin, user01, user02, user03,user04, user05,user06,user07,user08,user09,user10));
 
         System.out.println("======TEST USER ACCOUNTS INSERTED======");
     }
--- cpa/src/main/java/com/is442project/cpa/booking/seeder/BookingSeeder.java
+++ cpa/src/main/java/com/is442project/cpa/booking/seeder/BookingSeeder.java
@@ -1,5 +1,7 @@
 package com.is442project.cpa.booking.seeder;
 
+import com.is442project.cpa.account.model.Borrower;
+import com.is442project.cpa.account.model.Role;
 import com.is442project.cpa.account.model.UserAccount;
 import com.is442project.cpa.account.model.UserAccountRepository;
 import com.is442project.cpa.booking.model.Booking;
@@ -13,7 +15,11 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @Configuration
 @Profile("dev")
@@ -34,39 +40,69 @@ public class BookingSeeder {
     }
 
     private void insertTestBooking(){
+
         UserAccount borrower1 = accountRepository.getReferenceById("joshua.zhangzy@gmail.com");
         UserAccount borrower2 = accountRepository.getReferenceById("mary@nysi.org.sg");
-        UserAccount borrower3 = accountRepository.getReferenceById("david@sportsschool.edu.sg");
-        UserAccount borrower4 = accountRepository.getReferenceById("brotherbear@gmail.com");
+
 
         CorporatePass corporatePass1 = corporatePassRepository.findById(Long.parseLong("1")).get();
         CorporatePass corporatePass2 = corporatePassRepository.findById(Long.parseLong("2")).get();
-        CorporatePass corporatePass5 = corporatePassRepository.findById(Long.parseLong("5")).get();
-        CorporatePass corporatePass6 = corporatePassRepository.findById(Long.parseLong("6")).get();
-        CorporatePass corporatePass7 = corporatePassRepository.findById(Long.parseLong("7")).get();
-        CorporatePass corporatePass8 = corporatePassRepository.findById(Long.parseLong("8")).get();
-        
-        Booking booking1 = new Booking(LocalDate.of(2022, 11, 12), borrower1, corporatePass1, BookingStatus.COLLECTED);
-        Booking booking2 = new Booking(LocalDate.of(2022, 11, 14), borrower1, corporatePass2, BookingStatus.CONFIRMED);
-        Booking booking3 = new Booking(LocalDate.of(2022, 11, 17), borrower1, corporatePass2, BookingStatus.COLLECTED);
-        Booking booking4 = new Booking(LocalDate.of(2022, 10, 18), borrower2, corporatePass1, BookingStatus.COLLECTED);
-        Booking booking5 = new Booking(LocalDate.of(2022, 10, 18), borrower2, corporatePass2, BookingStatus.COLLECTED);
-        Booking booking6 = new Booking(LocalDate.of(2022, 10, 19), borrower2, corporatePass2, BookingStatus.CONFIRMED);
-        Booking booking7 = new Booking(LocalDate.of(2022, 11, 06), borrower2, corporatePass5, BookingStatus.CANCELLED);
-        Booking booking8 = new Booking(LocalDate.of(2022, 11, 11), borrower2, corporatePass5, BookingStatus.CANCELLED);
-        Booking booking9 = new Booking(LocalDate.of(2022, 11, 11), borrower2, corporatePass7, BookingStatus.CONFIRMED);
-        Booking booking10 = new Booking(LocalDate.of(2022, 11, 13), borrower3, corporatePass5, BookingStatus.COLLECTED);
-        Booking booking11 = new Booking(LocalDate.of(2022, 11, 15), borrower3, corporatePass5, BookingStatus.COLLECTED);
-        Booking booking12 = new Booking(LocalDate.of(2022,10,12), borrower4, corporatePass6, BookingStatus.DUESOWED, 54.0);
-        Booking booking13 = new Booking(LocalDate.of(2022,12,13), borrower4, corporatePass1, BookingStatus.CONFIRMED);
-        Booking booking14 = new Booking(LocalDate.of(2022,12,14), borrower2, corporatePass1, BookingStatus.CONFIRMED);
-        Booking booking15 = new Booking(LocalDate.of(2022,12,15), borrower2, corporatePass8, BookingStatus.CONFIRMED);
-        Booking booking100 = new Booking(LocalDate.of(2022, 12, 16), borrower1, corporatePass1, BookingStatus.CONFIRMED);
+        CorporatePass corporatePass3 = corporatePassRepository.findById(Long.parseLong("2")).get();
 
-        bookingRepository.saveAllAndFlush(Arrays.asList(booking1, booking2, booking3, booking4, booking5, booking6, booking7, booking8, booking9, booking10, booking11, booking12, booking13, booking14, booking15, booking100));
+        List<Booking> bookingList = new ArrayList<>();
+
+        Booking booking1 = new Booking(LocalDate.of(2022, 11, 18), borrower1, corporatePass1, BookingStatus.CONFIRMED);
+        Booking booking2 = new Booking(LocalDate.of(2022, 11, 18), borrower2, corporatePass2, BookingStatus.CONFIRMED);
+        bookingList.add(booking1);
+        bookingList.add(booking2);
+
+        Random random = new Random();
+        List<UserAccount> users = accountRepository.findAll();
+        users = users.stream().filter(userAccount -> userAccount.getRoles().contains(new Borrower())).collect(Collectors.toList());
+        List<CorporatePass> passes = corporatePassRepository.findAll();
+
+        Booking randomBooking;
+        for (int month = 1; month <= 10 ; month++) {
+            for (int day = 1; day <= 28; day++) {
+                for (int i = 0; i < random.nextInt(passes.size()); i++) {
+                    randomBooking = new Booking(LocalDate.of(2022, month, day), users.get(random.nextInt(users.size())), passes.get(i), BookingStatus.RETURNED);
+                    bookingList.add(randomBooking);
+                }
+            }
+        }
+
+        bookingList.get(50).setCorporatePass(corporatePass3);
+        bookingList.get(50).setBookingStatus(BookingStatus.DUESOWED);
+        bookingList.get(50).setFeesOwed(53.50);
+
+        for (int i = 51; i < bookingList.size(); i++) {
+            if (bookingList.get(i).getCorporatePass().getId() == corporatePass3.getId()) {
+                bookingList.get(i).setBookingStatus(BookingStatus.CANCELLED);
+            }
+        }
+
+
+//        Booking booking3 = new Booking(LocalDate.of(2022, 7, 2), borrower1, corporatePass2, BookingStatus.RETURNED);
+//        Booking booking4 = new Booking(LocalDate.of(2022, 7, 3), borrower1, corporatePass1, BookingStatus.RETURNED);
+//        Booking booking5 = new Booking(LocalDate.of(2022, 7, 5), borrower2, corporatePass2, BookingStatus.RETURNED);
+//        Booking booking6 = new Booking(LocalDate.of(2022, 7, 7), borrower2, corporatePass2, BookingStatus.RETURNED);
+//
+//        Booking booking7 = new Booking(LocalDate.of(2022, 1, 16), borrower2, corporatePass1, BookingStatus.RETURNED);
+//        Booking booking8 = new Booking(LocalDate.of(2022, 1, 18), borrower2, corporatePass1, BookingStatus.RETURNED);
+//        Booking booking9 = new Booking(LocalDate.of(2022, 1, 1), borrower2, corporatePass2, BookingStatus.CONFIRMED);
+//        Booking booking10 = new Booking(LocalDate.of(2022, 1, 2), borrower1, corporatePass2, BookingStatus.COLLECTED);
+//
+//        Booking booking11 = new Booking(LocalDate.of(2022, 11, 15), borrower1, corporatePass1, BookingStatus.COLLECTED);
+//        Booking booking12 = new Booking(LocalDate.of(2022,10,12), borrower2, corporatePass2, BookingStatus.DUESOWED, 54.0);
+//        Booking booking13 = new Booking(LocalDate.of(2022,12,13), borrower2, corporatePass1, BookingStatus.CONFIRMED);
+//        Booking booking14 = new Booking(LocalDate.of(2022,12,14), borrower2, corporatePass1, BookingStatus.CONFIRMED);
+//        Booking booking15 = new Booking(LocalDate.of(2022,12,15), borrower2, corporatePass2, BookingStatus.CONFIRMED);
+//        Booking booking100 = new Booking(LocalDate.of(2022, 12, 16), borrower1, corporatePass1, BookingStatus.CONFIRMED);
+
+        bookingRepository.saveAllAndFlush(bookingList);
 
         System.out.println("======TEST BOOKINGS INSERTED======");
 
     }
-    
+
 }
